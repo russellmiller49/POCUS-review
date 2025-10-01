72d2dbd1fff17384801e2430dd26b044f2cc773a
Russellmiller <russellmiller49@gmail.com>
2025-10-01 16:21:46 -0700

    Initial Commit

---
+++ POCUS_Mentor/POCUS_MentorApp.swift
@@ -0,0 +1,17 @@
+//
+//  POCUS_MentorApp.swift
+//  POCUS_Mentor
+//
+//  Created by Russell Miller on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct POCUS_MentorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
